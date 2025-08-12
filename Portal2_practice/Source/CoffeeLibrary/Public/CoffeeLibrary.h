0fca3e162c16d9dd7f5012c1bc315a0551608971
dopple <kuberin_b@naver.com>
2025-08-12 23:10:25 +0900

    Feat : CPP PlatfowmSwitch

---
+++ Portal2_practice/Source/CoffeeLibrary/Public/CoffeeLibrary.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2025 Doppleddiggong. All rights reserved. Unauthorized copying, modification, or distribution of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UCoffeeCommonUtil.h"
---
+++ Portal2_practice/Source/Portal2_practice/Common/Private/ULog.cpp
@@ -0,0 +1,3 @@
+#include "ULog.h"
+
+DEFINE_LOG_CATEGORY(ULog);
