1223d508c65e5036ef1e20c24665a576e159d5a8
dopple <kuberin_b@naver.com>
2025-08-21 23:32:21 +0900

    Feat : 기믹 오브젝트 추가

--- Portal2_practice/Source/CoffeeLibrary/Public/Features/UGameEventManager.h
+++ Portal2_practice/Source/CoffeeLibrary/Public/Features/UGameEventManager.h
@@ -13,10 +13,20 @@ class COFFEELIBRARY_API UGameEventManager : public UGameInstanceSubsystem
 public:
 	DEFINE_SUBSYSTEM_GETTER_INLINE(UGameEventManager);
 
+
 	DECLARE_MULTICAST_DELEGATE_OneParam(FOnScore, int);
 	FOnScore OnScore;
+
 
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnKill, int, Kill);
 	UPROPERTY(BlueprintAssignable, Category="Events")
 	FOnKill OnKill;
+
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMessage, FString, Msg);
+	UPROPERTY(BlueprintAssignable, Category="Events")
+	FOnMessage OnMessage;
+	
+	UFUNCTION(BlueprintCallable, Category="Events")
+	void SendMessage(const FString& InMsg);
 };
