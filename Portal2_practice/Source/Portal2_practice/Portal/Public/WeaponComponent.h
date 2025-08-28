80cadece7b4d85d5c04f2a341debe20128d63e7b
jeonghayoung <hyin0128@gmail.com>
2025-08-28 16:55:29 +0900

    Add 배경음 추가, 캐릭터 코드 수정중

--- Portal2_practice/Source/Portal2_practice/Portal/Public/WeaponComponent.h
+++ Portal2_practice/Source/Portal2_practice/Portal/Public/WeaponComponent.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TaskSyncManager.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "WeaponComponent.generated.h"
 
@@ -14,6 +15,11 @@ class PORTAL2_PRACTICE_API UWeaponComponent : public USkeletalMeshComponent
 {
 	GENERATED_BODY()
 public:
+
+	virtual void BeginPlay() override;
+
+
+	
 	// 변수 선언
 	// FirstPersonCharacter.h (플레이어에 보관할 포인터는 UPROPERTY로!)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Weapon")
@@ -26,4 +32,11 @@ public:
 	// 변수 선언
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class UPrimitiveComponent* GrabbedObject;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class AFirstPersonCharacter* player;
+
+
+
+	
 };
