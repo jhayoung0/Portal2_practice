b71b6ea0e9f0d6f05140611bb41c24e27c88ad0f
jeonghayoung <hyin0128@gmail.com>
2025-08-28 17:45:18 +0900

    cpp변경: 플레이어 메쉬컴포넌트 cpp로 변경

--- Portal2_practice/Source/Portal2_practice/Portal/Private/FirstPersonCharacter.cpp
+++ Portal2_practice/Source/Portal2_practice/Portal/Private/FirstPersonCharacter.cpp
@@ -2,12 +2,44 @@
 
 
 #include "FirstPersonCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
+
 
 // Sets default values
 AFirstPersonCharacter::AFirstPersonCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// 카메라 설정
+	FirstPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCamera->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCamera->SetRelativeLocation(FVector(30.f, 0.f,60.f));
+	FirstPersonCamera->bEditableWhenInherited = true;
+
+	
+	// firstperson mesh 설정
+	FirstPersonMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	FirstPersonMesh->SetupAttachment(FirstPersonCamera);
+	FirstPersonMesh->SetRelativeLocation(FVector(-22.f, -5.f, -148.f));
+	FirstPersonMesh->bEditableWhenInherited = true;
+	
+	/* HL, HR
+	HL = CreateDefaultSubobject<USceneComponent>(TEXT("HL"));
+	HL->SetupAttachment(FirstPersonMesh);
+	HL->SetRelativeLocation(FVector(-35.f, 3.f, 11.f));
+	HL->bEditableWhenInherited = true;
+
+	
+	HR = CreateDefaultSubobject<USceneComponent>(TEXT("HR"));
+	HR->SetupAttachment(FirstPersonMesh);
+	HR->SetRelativeLocation(FVector(10.f, 0.f, 0.f));
+	HR->bEditableWhenInherited = true;
+	*/
+
+	
 
 }
 
